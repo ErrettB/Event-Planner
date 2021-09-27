40bf9f4d77f89078311d4f88b968180b8fd65277
Errett Bainum <51064279+ErrettB@users.noreply.github.com>
2021-09-27 09:32:45 -0400

    Update Login.tsx
    Add skeleton login page

--- event-planner-app/web-react-ts/src/components/Login.tsx
+++ event-planner-app/web-react-ts/src/components/Login.tsx
@@ -1,10 +1,26 @@
 import {React} from 'React'
-
+import { useForm } from "react-hook-form"
+import { useQuery } from 'graphql-hooks'
 
 export default function Login() {
-
-return(
+  const {register, handleSubmit} = useForm();
+  const onSubmit = (d) =>
+    alert(JSON.stringify(d));
   
+  
+return(
+  <h1>Login Page</h1>
+  <form onSubmit={handleSubmit(onSubmit)}>
+    <label>
+    First Name:
+    <input {...register("firstName")}>
+    </label>
+    
+    <label>
+      Last Name:
+      <input {...register("lastName")}>
+    </label>
+      </form>
 
 )
 
