90a39c8bd51f2a7d828977d431db3985377c4e1f
Errett Bainum <errettbainum@gmail.com>
2021-11-11 21:43:14 -0500

    Squashed commit of the following:
    commit b7ac844c7c3be3e3b1affb84aa2afc4e98710893
    Author: Errett Bainum <errettbainum@gmail.com>
    Date:   Thu Nov 11 21:42:33 2021 -0500
    
        changes
    
    commit e9c48cc94c19d83e298cc15ee670435e357d5b35
    Author: Errett Bainum <errettbainum@gmail.com>
    Date:   Thu Nov 11 21:13:08 2021 -0500
    
        added changes to brnach
    
    commit aa8f478518926fd83934d18164e3b03f4724fd3e
    Author: Errett Bainum <errettbainum@gmail.com>
    Date:   Tue Oct 26 13:14:52 2021 -0400
    
        created functionality of reminders system
    
    commit 401fedfe099828b0ed723986196a9186c1bc5609
    Author: Errett Bainum <errettbainum@gmail.com>
    Date:   Sat Oct 23 20:45:15 2021 -0400
    
        added changes
    
    commit 4cdc1d6e73b29fe17761eaae32c65f5652cec293
    Author: Errett Bainum <errettbainum@gmail.com>
    Date:   Sat Oct 23 17:58:42 2021 -0400
    
        changes to homepages
    
    commit c5415da21db15bb6093519428ffd418ac8363d21
    Author: Errett Bainum <errettbainum@gmail.com>
    Date:   Fri Oct 22 10:05:35 2021 -0400
    
        changed styles sheet
    
    commit de1ecf86b5a952510a8264a100d695c4d82aec28
    Merge: 5359086 3fc6776
    Author: Errett Bainum <errettbainum@gmail.com>
    Date:   Fri Oct 22 10:05:23 2021 -0400
    
        Merge commit '3fc67761200f6e7b28044f796422fc31d1f1ff81' into origin/Feat-dashboard-layout
    
    commit 5359086f0db79ab17a128c339732e4d01b98755b
    Author: Errett Bainum <errettbainum@gmail.com>
    Date:   Thu Oct 21 20:45:26 2021 -0400
    
        changed styles sheet
    
    commit 922ad6f3b41157a3694b4a861e9a5c276c59d8a6
    Author: Errett Bainum <51064279+ErrettB@users.noreply.github.com>
    Date:   Sat Oct 16 18:41:16 2021 -0400
    
        Update App.tsx
    
        Gutted the default app page to a clean page
    
    commit 46cd37e1c8449d914d491efd432608e1f09ba7b9
    Author: Errett Bainum <51064279+ErrettB@users.noreply.github.com>
    Date:   Sat Oct 16 18:32:17 2021 -0400
    
        Update App.tsx
    
        Removed style sheet and imported them in dedicated style page
    
    commit 924afb57d9429afa1fe39668fb16563d615b3f43
    Author: Errett Bainum <51064279+ErrettB@users.noreply.github.com>
    Date:   Sat Oct 16 18:28:40 2021 -0400
    
        Update Styles.css
    
        Imported styles from default app file to new file
    
    commit e7aa2333903f4ad167855bbe575d8aefab288872
    Author: Errett Bainum <51064279+ErrettB@users.noreply.github.com>
    Date:   Sat Oct 16 18:24:00 2021 -0400
    
        Create Styles.css
    
        Add Master Css styles sheet

---
+++ event-planner-app/web-react-ts/src/components/TodoForm.tsx
@@ -0,0 +1,104 @@
+import React, { ChangeEvent, useEffect, useState } from 'react'
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  TextField,
+} from '@material-ui/core'
+import { useForm } from 'react-hook-form'
+import { empty } from '@apollo/client'
+import TodoList from './TodoList'
+import { IToDo } from '../Interfaces'
+
+
+
+export default function TodoForm() {
+  const [task, setTask] = useState('')
+  const [deadline, setDeadline] = useState(new Date())
+  const [toDos, setToDos] = useState<IToDo[]>([])
+  const [modalButton, setModalButton] = useState(false)
+  const [deadlineButton, setDeadlineButton] = useState(false)
+  const [rerender, setRerender] = useState(false)
+
+  const addToDo = (): any => {
+    const newTask = { nameToDo: task }
+    setToDos([newTask, ...toDos])
+    setModalButton(!modalButton)
+
+    console.log(toDos)
+  }
+
+  const handleDeadline = (): any => {
+    setDeadline(deadline)
+  }
+
+  const handleChangeTask = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === 'task') {
+      setTask(event.target.value)
+    }
+  }
+
+  const handleChangeDeadline = (event: Date): void => {
+    setDeadline(event)
+    console.log(event)
+  }
+
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   watch,
+  //   formState: { errors },
+  // } = useForm<ToDoInputs>()
+
+  const handleClose = () => {
+    setModalButton(false)
+  }
+  const handleDeadlineButton = () => {
+    setDeadlineButton(!deadlineButton)
+  }
+
+  return (
+    <div>
+      <Button variant="contained" onClick={() => setModalButton(!modalButton)}>
+        Create new To-Do
+      </Button>
+
+      
+
+      {modalButton && (
+        <Dialog
+          fullWidth
+          maxWidth="md"
+          open={modalButton}
+          onClose={handleClose}
+        >
+          <form>
+            <DialogContent>
+              <TextField
+                fullWidth
+                id="outlined-basic"
+                label="What do you need to-do?"
+                onChange={handleChangeTask}
+                name="task"
+                value={task}
+              ></TextField>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleDeadlineButton}> Create a Deadline</Button>
+              <Button onClick={handleClose}>Cancel</Button>
+              <Button type="submit" onClick={addToDo}>
+                Submit
+              </Button>
+            </DialogActions>
+          </form>
+        </Dialog>
+      )}
+      <div>
+        {toDos.map((todo: IToDo, key: number) => {
+          return <TodoList key={key} task={todo} />
+        })}
+      </div>
+    </div>
+  )
+}
