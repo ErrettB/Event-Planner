0ac3b09789ba48c0255bab36d70d9eddb1775374
ErrettBainum_Mac <errettbainum@gmail.com>
2021-11-26 13:28:32 -0500

    added grid system to about page

--- event-planner-app/web-react-ts/src/components/About.tsx
+++ event-planner-app/web-react-ts/src/components/About.tsx
@@ -1,4 +1,4 @@
-import { Box, makeStyles, withStyles, Typography, Paper, createTheme, ThemeProvider, styled  } from '@material-ui/core'
+import { Box, makeStyles, withStyles, Typography, Paper, createTheme, ThemeProvider, styled, Grid  } from '@material-ui/core'
 import React from 'react'
 
 
@@ -6,7 +6,7 @@ import React from 'react'
 export default function About() {
     const useStyles = makeStyles((theme) => ({
         root: {
-          height: '100vh',
+          height: '100%',
           backgroundImage: "linear-gradient(45deg, #000000, 20%, #1d007d, 80%, #3d74ff)",
           margin: "0px",
           padding: '0px'
@@ -24,11 +24,19 @@ export default function About() {
           width: '50%',
           margin: '10px',
         },
+        grid2:{
+          width:'100%'
+        },
         textfield: {
           margin: ' 40px 0px 40px 0px',
         },
+        smallBox:{
+          width:"100%"
+        }
       }))
       const classes = useStyles()
+      const className = useStyles()
+      
 
       const Item = styled(Paper)(({ theme}) => ({
         textAlign: 'center'
@@ -74,18 +82,55 @@ export default function About() {
         flexDirection="column"        
         alignContent="center"        
         width='90vw'
-        padding="10vh 0 10vh 0"      
+        padding="20vh 0 20vh 0"      
         >
           
           <WhiteTextTypography align="center" variant="h1" gutterBottom >
             The perfect date for you
             </WhiteTextTypography>
             <SubTextTypography variant="h4" align="center" gutterBottom>
-              Get on the same page with your <BlackTextTypography  >Friends, Family and Collegues</BlackTextTypography>
+              Get on the same page with your <BlackTextTypography >Friends, Family and Collegues.</BlackTextTypography>
               </SubTextTypography>
               
             </Box>
             </Box> 
+
+            <Box p={2}>
+            
+            <Grid 
+            container             
+            xs = {12}        
+            >            
+              <Grid container                           
+                xs = {6}
+                justifyContent='flex-end'               
+                >                
+                <Grid item xs={12} >
+                <Paper >
+                  Team
+                  </Paper>
+                </Grid>
+
+                <Grid item xs = {12}>
+                  <Paper >
+                  Tech
+                  </Paper>
+                </Grid>
+              </Grid>
+
+                <Grid item xs = {6}>
+                    <Paper style={{height:'100%'}}>
+                      Test
+                      </Paper>
+                  </Grid>
+                
+                <Grid item xs={12} >
+                  <Paper> 
+                    Lower bar
+                  </Paper>
+                  </Grid>
+            </Grid>
+            </Box>         
         </div>
     )
 }
