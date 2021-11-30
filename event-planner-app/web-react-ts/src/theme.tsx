971a04626ce3c54f2e37ed57fa7182b74d99a8bd
Errett Bainum <errettbainum@gmail.com>
2021-11-29 20:56:35 -0500

    test

--- event-planner-app/web-react-ts/src/theme.tsx
+++
@@ -1,9 +0,0 @@
-import { createTheme } from "@material-ui/core";
-
-const theme = createTheme({
-    typography:{
-        fontFamily:['Lato', 'DM Serif Display'].join(',')
-    }
-})
-
-export default theme;
