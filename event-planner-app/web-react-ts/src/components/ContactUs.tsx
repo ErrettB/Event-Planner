8582e326b878b23e375882d75f2a00a2a7b4f7e1
ErrettBainum_Mac <errettbainum@gmail.com>
2021-12-01 20:45:52 -0500

    contact page update

--- event-planner-app/web-react-ts/src/components/ContactUs.tsx
+++ event-planner-app/web-react-ts/src/components/ContactUs.tsx
@@ -1,10 +1,14 @@
+import { ThemeProvider } from '@material-ui/core'
 import React from 'react'
-import ThemeProvider
+import theme from '../theme'
+
 
 export default function ContactUs() {
     return (
+        <ThemeProvider theme={theme}>
         <div>
             Hello
         </div>
+        </ThemeProvider>
     )
 }
