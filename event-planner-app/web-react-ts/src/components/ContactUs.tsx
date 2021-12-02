1d67b6b8d365ab5c84cec210f219d96d677125fe
Errett Bainum <errettbainum@gmail.com>
2021-12-01 23:47:47 -0500

    added content to grid

--- event-planner-app/web-react-ts/src/components/ContactUs.tsx
+++ event-planner-app/web-react-ts/src/components/ContactUs.tsx
@@ -1,44 +1,54 @@
-import { Container, CssBaseline, Grid, Paper, ThemeProvider } from '@material-ui/core'
+import { Box, Container, CssBaseline, Grid, Paper, styled, ThemeProvider } from '@material-ui/core'
 import React from 'react'
 import theme from '../theme'
 
 
+const ContactBox = styled(Paper)(({theme}) => ({
+    minHeight: '50%',
+    margin: '10%',
+    palette:{
+        type: 'white'
+    }
+}))
+
+const InnertBox = styled(Paper)(({theme}) => ({
+    minWidth: '50%',
+    marginBottom: 
+}))
+
+const rows = [
+{name: 'Errett Bainum', age: '21', email:'ebainum@citadel.edu'}, 
+{name: 'Errett Bainum', age: '21', email:'ebainum@citadel.edu'}, 
+{name: 'Errett Bainum', age: '21', email:'ebainum@citadel.edu'}, 
+{name: 'Errett Bainum', age: '21', email:'ebainum@citadel.edu'}, 
+{name: 'Errett Bainum', age: '21', email:'ebainum@citadel.edu'}, 
+{name: 'Errett Bainum', age: '21', email:'ebainum@citadel.edu'}]
+
 export default function ContactUs() {
+
+
     return (
         <ThemeProvider theme={theme}>
-            <Container component='main' maxWidth='lg'>
-                <CssBaseline/>
-                <Grid container xs={12} >
-                    <Grid container xs={6} >
-                        <Grid item xs={12}>
-                            <Paper>
-                                1
-                            </Paper>
-                            </Grid>
-
-                        <Grid item xs={12}>
-                            <Paper>2</Paper>
-                            </Grid>
-
-                        <Grid item xs={12}>
-                            <Paper>3</Paper>
-                            </Grid>
-                        </Grid>
-                    
-                    <Grid container xs={6} >
-
-                    <Grid item xs={12}>
-                    <Paper>4</Paper>
-                            </Grid>
-                    <Grid item xs={12}>
-                    <Paper>5</Paper>
-                            </Grid>
-                    <Grid item xs={12}>
-                    <Paper>6</Paper>
-                            </Grid>
-                        </Grid>
-                    </Grid>        
+            <CssBaseline/>
+            <Container disableGutters maxWidth={false} >
+                <Box m={0}>                
+                <Grid 
+                container xs={12}
+                spacing={0}               
+                style= {{minHeight: '100vh'}}                
+                 >
+                  {rows.map(row =>(
+                      <Grid item xs={6}>
+                      <ContactBox>
+                          <InnertBox>{row.name}</InnertBox>
+                          <InnertBox>{row.age}</InnertBox>
+                          <InnertBox>{row.email}</InnertBox>  </ContactBox>
+                              </Grid>
+                  ))}                          
+                    </Grid>
+                    </Box>          
             </Container>
+            
         </ThemeProvider>
     )
 }
