8b63bcd09f1818b804c25aeb3c719d744474fde4
Errett Bainum <errettbainum@gmail.com>
2021-12-03 02:10:59 -0500

    added img to folder

--- event-planner-app/web-react-ts/src/components/ContactUs.tsx
+++ event-planner-app/web-react-ts/src/components/ContactUs.tsx
@@ -1,29 +1,30 @@
-import { Box, Container, CssBaseline, Grid, Paper, styled, ThemeProvider, Typography } from '@material-ui/core'
+import { Box, Card, CardMedia, Container, CssBaseline, Grid, Paper, styled, ThemeProvider, Typography } from '@material-ui/core'
 import React from 'react'
 import theme from '../theme'
 
 
-const ContactBox = styled(Paper)(({theme}) => ({
+
+const ContactBox = styled(Card)(({theme}) => ({
     minHeight: '50%',
-    margin: '10%',
+    margin: '10px',
     palette:{
         type: 'white'
     }
 }))
 
-const InnertBox = styled(Paper)(({theme}) => ({
+const InnertBox = styled(Card)(({theme}) => ({
     minWidth: '50%',
-    margin: '5%'
+    margin: '10px'
 
 }))
 
 const rows = [
-{name: 'Errett Bainum', age: '21', email:'ebainum@citadel.edu'}, 
-{name: 'Errett Bainum', age: '21', email:'ebainum@citadel.edu'}, 
-{name: 'Errett Bainum', age: '21', email:'ebainum@citadel.edu'}, 
-{name: 'Errett Bainum', age: '21', email:'ebainum@citadel.edu'}, 
-{name: 'Errett Bainum', age: '21', email:'ebainum@citadel.edu'}, 
-{name: 'Errett Bainum', age: '21', email:'ebainum@citadel.edu'}]
+{name: 'Harrison Martin', age: '21', phone:'9102658553', email:'hmartin1@citadel.edu', img:'../imgs/Ben'}, 
+{name: 'Mathew Daugomah', age: '21', phone:'8438146548', email:'P00Nslayer6969@gmail.com', img:'../imgs/Jalen.jpg'}, 
+{name: 'Benjamin Whitlock', age: '21', phone:'8032107403', email:'bwhitloc@citadel.edu', img:'../imgs/Ben.jpg'}, 
+{name: 'Jalen Singleton', age: '21', phone:'911',email:'Jsingle3@citadel.edu', img:'../imgs/Jalen.jpg'}, 
+{name: 'Jakob Schumacher ', age: '22', phone:'7734123879', email:'Jschumac@citadel.edu', img:'./imgs/Jacob.jpg'}, 
+{name: 'Errett Bainum', age: '21', phone:'8433432139', email:'ebainum@citadel.edu', img:''}]
 
 export default function ContactUs() {
 
@@ -35,15 +36,26 @@ export default function ContactUs() {
                 <Box m={0}>                
                 <Grid 
                 container xs={12}
-                spacing={0}               
+                spacing={2}               
                 style= {{minHeight: '100vh'}}                
                  >
                   {rows.map(row =>(
+                      
                       <Grid item xs={6}>
-                      <ContactBox>
+                      <Card>
+                      {/* <CardMedia
+                      component="img"
+                      height="194"
+                      image={require(row.img)}
+                      alt="Paella dish"/>   */}
+                      <img
+                        src={`${row.img}`}
+                        height='100px'
+                      />
                           <InnertBox> <Typography variant='h4'>{row.name}</Typography></InnertBox>
                           <InnertBox><Typography variant='h4'>{row.age}</Typography></InnertBox>
-                          <InnertBox><Typography variant='h4'>{row.email}</Typography></InnertBox>  </ContactBox>
+                          <InnertBox><Typography variant='h4'>{row.email}</Typography></InnertBox>
+                          </Card>
                               </Grid>
                   ))}                          
                     </Grid>
